ab865f710faa7eac9a31a3bb95cb30ce2c669b87
Tachi <zaveruha007@gmail.com>
2015-06-04 17:32:45 +0300

    CSS frontend

--- js/slider.js
+++ js/slider.js
@@ -2,5 +2,9 @@
  * @author Gleb Zaveruha
  */
 $(document).ready(fucntion(){
+	//Options
+	var speed = 500; 				//Fade speed
+	var autoswitch = true; 			//Autoslider option
+	var autoswitch_speed = 4000; 	//Auto Slider Speed
 	
 });
